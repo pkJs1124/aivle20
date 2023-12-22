43773960c8bff76f574a29a256a46971deb7aad4
bste1220 <bste1146@gmail.com>
2023-12-22 13:23:15 +0900

    comutem

--- guardsite/communities/urls.py
+++ guardsite/communities/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create/',views.create, name ='create'),
     path('<int:notice_pk>/',views.detail, name='detail'),
     path('<int:notice_pk>/delete/',views.delete, name= 'delete'),
-    path('<int:notice_pk/update/',views.update, name='update'),
+    path('<int:notice_pk>/update/',views.update, name='update'),
     
     
 ]
