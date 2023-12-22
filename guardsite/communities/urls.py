0a726b30c0a4ea46e080fe8bb75821e1e4d557f2
bste1220 <bste1146@gmail.com>
2023-12-22 10:39:09 +0900

    notice views

--- guardsite/communities/urls.py
+++ guardsite/communities/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/',views.create, name ='create'),
     path('<int:notice_pk>/',views.detail, name='detail'),
-    
+    path('<int:notice_pk>/delete/',views.delete, name= 'delete'),
+    path('<int:notice_pk/update/',views.update, name='update'),
     
     
 ]
