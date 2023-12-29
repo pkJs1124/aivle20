290fa015fda0282359988b91f38739a73e6a84af
Woojin-02 <waw1159@gmail.com>
2023-12-29 15:18:25 +0900

    showreport

--- guardsite/showreport/views.py
+++ guardsite/showreport/views.py
@@ -1,14 +1,20 @@
 # views.py
 
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, redirect
+from django.urls import reverse
 import openai
+from datetime import datetime
+from .models import ChecklistEntry
+from .forms import ChecklistForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import F
+from django.shortcuts import get_object_or_404
 
 # OpenAI API 키 설정
-openai.api_key = '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'
+openai.api_key = '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'
 
 
-def get_openai_results(prompt):
+def get_openai_results(request):
     # GPT 모델에 입력할 텍스트 생성
     # prompt 변수에는 checklist_item에 해당하는 정보가 들어갑니다.
     prompt_text = f"""아래의 문항이 제대로 이뤄졌는지, true, false를 답변해줘. user 각각의 true/false 여부는 필요없어. user의 내역을 참고해서 모든 유저의 데이터가 체크사항에 적합하면 true, 아니면 false라고 쓰면 돼.
@@ -44,20 +50,73 @@ def get_openai_results(prompt):
     print(user_messages)
 
     # GPT 모델에 텍스트 전달하여 문장 생성
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",  # GPT 모델 엔진 선택
-    #     messages=user_messages,
-    #     temperature=0.2,  # 문장 다양성 조절
-    #     max_tokens=1024  # 생성 문장의 최대 길이
-    # )
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # GPT 모델 엔진 선택
+        messages=user_messages,
+        temperature=0.2,  # 문장 다양성 조절
+        max_tokens=1024  # 생성 문장의 최대 길이
+    )
 
-    # # 생성된 문장에서 "True" 또는 "False"를 추출
-    # generated_text = response['choices'][0]['message']['content'].strip().lower()
-    # return generated_text == "true"
+    # 생성된 문장에서 "True" 또는 "False"를 추출
+    generated_text = response['choices'][0]['message']['content'].strip().lower()
+    
+    print(generated_text)
+    
+    save_to_database(generated_text)
+    
+    current_date = datetime.now().date()
+
+    url = reverse("showreport:display_checklist", kwargs={'date': current_date})
+
+    # 리다이렉트 함수 호출 시에는 URL을 첫 번째 인자로, 두 번째 인자로는 kwargs를 사용합니다.
+    return redirect(url)
+    
+    
+def save_to_database(generated_text):
+    # 현재 날짜 가져오기
+    current_date = datetime.now().date()
+
+    # '\n'을 기준으로 문장을 분리
+    sentences = generated_text.split('\n')
+    
+    print(sentences)
+
+    # 각 문장에서 'true' 또는 'false' 값을 추출하여 저장
+    for sentence in sentences:
+        # 문장에서 ': '을 기준으로 분리하여 필드와 값으로 나눔
+        try:
+            field, value_str = sentence.split(': ', 1)
+            field = field.strip()
+            value_str = value_str.strip()
+        except ValueError:
+            # 예외가 발생하면 (split이 제대로 이뤄지지 않으면) 건너뜁니다.
+            continue
+
+        # 'true' 또는 'false' 값을 추출하여 Boolean으로 변환
+        value = value_str.lower() == 'true'
+
+        # ChecklistEntry 모델을 사용하여 DB에 저장
+        entry = ChecklistEntry(
+            create_at=current_date,
+            truefalse=value
+        )
+        entry.save()
+
+    print("Result saved to database.")
+    
+    
+def checklist_board(request, page=1):
+    items_per_page = 10
+    start_index = (page - 1) * items_per_page
+    end_index = start_index + items_per_page
+
+    # 중복 제거 및 정렬된 날짜 가져오기
+    dates = ChecklistEntry.objects.values('create_at').distinct().order_by('-create_at')
+
+    return render(request, 'report/reportlist.html', {'checklist_entries': dates, 'page': page})
 
 
-
-def display_checklist(request):
+def display_checklist(request, date):
     checklist_items = [
         {"name": "작업에 적합한 보호구 지급 및 착용, 안전교육 실시 여부"},
         {"name": "작업별 안전수칙 준수여부 (위험요인 확인점검, 절차 준수, 안전시설 설치 등)"},
@@ -72,12 +131,34 @@ def display_checklist(request):
         {"name": "현장 정리정돈 상태"},
         {"name": "긴급 상황 대비 비상연락망 관리 상태"},
     ]
-    
+
+    # 날짜에 해당하는 데이터 가져오기
+    checklist_entries = ChecklistEntry.objects.filter(create_at=date)
+
     results = []
 
-    for item in checklist_items:
-        checked = get_openai_results(item["name"])
-        item["checked"] = checked
-        results.append(item)
+    for i, entry in enumerate(checklist_entries):
+        results.append({"name": checklist_items[i]["name"], "truefalse": entry.truefalse})
 
-    return render(request, 'report/report.html', {'checklist_items': results})
+    form = ChecklistForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            # 현재 날짜
+            current_date = datetime.now().date()
+            
+            # 해당 날짜에 대한 기존 데이터 가져오기
+            checklist_entry = get_object_or_404(ChecklistEntry, create_at=date)
+            
+            # 기존 데이터가 있으면 수정, 없으면 새로 생성
+            if checklist_entry:
+                form = ChecklistForm(request.POST, instance=checklist_entry)
+            else:
+                form = ChecklistForm(request.POST)
+
+            # 저장
+            form.save()
+
+    return render(request, 'report/report.html', {'checklist_items': results, 'current_date': date})
+
+    # return render(request, 'report/report.html', {'checklist_items': results})
