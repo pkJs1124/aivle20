3e60b28d7e94b68b826fe8235da91d79fc751207
pkJs1124 <127395920+pkJs1124@users.noreply.github.com>
2024-01-09 17:42:00 +0900

    css

--- guardsite/detectmodel/urls.py
+++ guardsite/detectmodel/urls.py
@@ -8,5 +8,6 @@ app_name = 'detectmodel'
 urlpatterns = [
    path('',views.index, name ='index'),
    path('upload/', views.danger_post,name ='danger_post'),
+   #path('<int:pk>/', views.danger_post, name='danger_post'),
    path('<int:danger_pk>/',views.detail, name='detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
