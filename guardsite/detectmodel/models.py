7e7896f866e12604462bda16fef723f380f5c85a
bste1220 <bste1146@gmail.com>
2024-01-09 10:54:39 +0900

    asdfa

--- guardsite/detectmodel/models.py
+++ guardsite/detectmodel/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class UploadedImage(models.Model):
     image = models.ImageField(upload_to='uploaded_images/')
+    
+    class Meta:
+        db_table = 'uploadimage'
